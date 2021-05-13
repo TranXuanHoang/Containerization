3011850d72b3d9320fd75985bd0872e8e5682e94
TranXuanHoang <hoangtx.ict@gmail.com>
2021-05-13 22:43:10 +0900

    Add Docker Compose and Docker config files

--- 01-visits-counter/README.md
+++ 01-visits-counter/README.md
@@ -1,3 +1,20 @@
 # Visits Counter
 
 Build a counter counting the number of user visits to a website.
+
+```powershell
+# Build and run all containers using Docker compose.
+# Run this command the first time to build new images or
+# to update new source code to the docker containers.
+01-visits-counter:~$ docker compose up --build
+
+# Then after the first build, run the following command
+# to start running containers without building them again.
+01-visits-counter:~$ docker compose up
+
+# Stop running containers
+01-visits-counter:~$ docker compose down
+
+# Check running containers
+01-visits-counter:~$ docker compose ps
+```
--- 01-visits-counter/index.js
+++ 01-visits-counter/index.js
@@ -1,16 +1,28 @@
 import express from 'express'
 import redis from 'redis'
+import process from 'process'
 
 const app = express()
-const client = redis.createClient()
-client.set('visits', 0)
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+})
+client.get('visits', (err, visits) => {
+  if (err || !visits) {
+    client.set('visits', 0)
+  }
+})
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send(`Number of visits is ${visits}`)
+    res.send(`Number of visits is: ${visits}`)
 
     client.set('visits', +visits + 1)
   })
+})
+
+app.get('/crash', (req, res) => {
+  process.exit(0)
 })
 
 app.listen(8081, () => {
