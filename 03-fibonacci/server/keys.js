9f01415003b0dd4d849094ad9e6eda3f664b1be4
TranXuanHoang <hoangtx.ict@gmail.com>
2021-07-08 00:21:26 +0900

    Initialize Fibonacci server microservice

---
+++ 03-fibonacci/server/keys.js
@@ -0,0 +1,10 @@
+// Environment variables for Redis DB connection
+export const redisHost = process.env.REDIS_HOST
+export const redisPort = process.env.REDIS_PORT
+
+// Environment variables for PostgreSQL DB connection
+export const pgUser = process.env.PGUSER
+export const pgHost = process.env.PGHOST
+export const pgDatabase = process.env.PGDATABASE
+export const pgPassword = process.env.PGPASSWORD
+export const pgPort = process.env.PGPORT
