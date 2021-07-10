fd14e25383e0f82bf4514f4f65d8f7a9ee8fa1e2
TranXuanHoang <hoangtx.ict@gmail.com>
2021-07-10 23:28:36 +0900

    Implement logic for the client app

--- 03-fibonacci/client/src/App.js
+++ 03-fibonacci/client/src/App.js
@@ -1,10 +1,27 @@
+import {
+  BrowserRouter as Router, Link, Route
+} from 'react-router-dom';
 import './App.css';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Fib Calculator</h3>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="Header">
+          <h2>Fib Calculator</h2>
+          <nav className="NavBar">
+            <Link to="/" className="App-Link">Home</Link>
+            <Link to="/otherpage" className="App-Link">Other Page</Link>
+          </nav>
+        </div>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
